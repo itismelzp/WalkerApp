331f56fb7bdf04bd37c00434dc43504418a66edb
walkerzpli <walkerzpli@tencent.com>
2020-09-15 17:11:03 +0800

    用drawable画背景

--- app/src/main/java/com/demo/customview/view/CustomFeedTailView.java
+++ app/src/main/java/com/demo/customview/view/CustomFeedTailView.java
@@ -10,6 +10,7 @@ import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -29,15 +30,17 @@ public class CustomFeedTailView extends View {
     public static final String TAG = "CustomFeedTailView";
 
     private Paint mPaint;
-    private Paint mGradientPaint;
+//    private Paint mGradientPaint;
     private TextPaint mTextPaint;
 
     private Bitmap mLeftIcon;
+    private Drawable mLeftIconDrawable;
     private int mLeftIconSize;
     private String mRightText;
     private Bitmap mRightIcon;
+    private Drawable mRightIconDrawable;
     private int mRightIconSize;
-    private Bitmap mBackGround;
+    private Drawable mBgDrawable;
     private String mTitle;
     private int mTitleColor;
     private int mTitleSize;
@@ -74,6 +77,7 @@ public class CustomFeedTailView extends View {
             switch (attr) {
                 case R.styleable.CustomFeedTailView_leftIcon:
                     mLeftIcon = BitmapFactory.decodeResource(getResources(), array.getResourceId(attr, 0));
+                    mLeftIconDrawable = getResources().getDrawable(array.getResourceId(attr, 0));
                     break;
                 case R.styleable.CustomFeedTailView_leftIconSize:
                     defSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
@@ -85,6 +89,7 @@ public class CustomFeedTailView extends View {
                     break;
                 case R.styleable.CustomFeedTailView_rightIcon:
                     mRightIcon = BitmapFactory.decodeResource(getResources(), array.getResourceId(attr, 0));
+                    mRightIconDrawable = getResources().getDrawable(array.getResourceId(attr, 0));
                     break;
                 case R.styleable.CustomFeedTailView_rightIconSize:
                     defSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
@@ -95,7 +100,7 @@ public class CustomFeedTailView extends View {
                     mTitle = array.getString(attr);
                     break;
                 case R.styleable.CustomFeedTailView_titleTextColor:
-                    mTitleColor = array.getIndex(attr);
+                    mTitleColor = array.getColor(attr, Color.BLACK);
                     break;
                 case R.styleable.CustomFeedTailView_titleTextSize:
                     defSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
@@ -103,7 +108,7 @@ public class CustomFeedTailView extends View {
                     mTitleSize = array.getDimensionPixelSize(attr, defSize);
                     break;
                 case R.styleable.CustomFeedTailView_background:
-//                    mBackGround = BitmapFactory.decodeResource(getResources(), array.getResourceId(attr, 0));
+                    mBgDrawable = getResources().getDrawable(array.getResourceId(attr, 0));
                     break;
                 default:
                     break;
@@ -112,7 +117,7 @@ public class CustomFeedTailView extends View {
 
         array.recycle();
         mPaint = new Paint();
-        mGradientPaint = new Paint();
+//        mGradientPaint = new Paint();
 
         mRect = new Rect(); // 整个view的边框
         mLeftIconRect = new Rect();
@@ -137,7 +142,7 @@ public class CustomFeedTailView extends View {
         if (specMode == MeasureSpec.EXACTLY) {
             mWidth = specSize;
         } else if (specMode == MeasureSpec.AT_MOST) {
-            int desireWidth = getPaddingLeft() + mLeftIcon.getWidth() + mTextBound.width() + mRightIcon.getWidth() + getPaddingRight();
+            int desireWidth = getPaddingLeft() + mLeftIconSize + mTextBound.width() + mRightIconSize + getPaddingRight();
             mWidth = Math.min(desireWidth, specSize);
         }
 
@@ -147,7 +152,7 @@ public class CustomFeedTailView extends View {
         if (specMode == MeasureSpec.EXACTLY) {
             mHeight = specSize;
         } else if (specMode == MeasureSpec.AT_MOST) {
-            int desireHeight = Math.max(mLeftIcon.getHeight(), mTextBound.height());
+            int desireHeight = Math.max(mLeftIconSize, mTextBound.height());
             mHeight = getPaddingLeft() + desireHeight + getPaddingRight();
         }
 
@@ -159,33 +164,48 @@ public class CustomFeedTailView extends View {
         super.onDraw(canvas);
 
         // draw bg
-        initBackGround(canvas, mPaint);
+        drawBackGround(canvas, mPaint);
         // draw left icon
-        initLeftIcon(canvas, mPaint);
+        drawLeftIcon(canvas, mPaint);
         // draw right icon
-        initRightIcon(canvas, mPaint);
+        drawRightIcon(canvas, mPaint);
+
+        // init paint
+        mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mTextPaint.setColor(mTitleColor);
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setStrokeWidth(0.5f); // 控制字体加粗的程度
         // draw right text
-        initRightText(canvas, mTextPaint);
+        drawRightText(canvas, mTextPaint);
         // draw text
-        initText(canvas, mTextPaint);
+        drawText(canvas, mTextPaint);
 
 //        drawProgress(canvas); // 进度条
     }
 
-    private void initBackGround(Canvas canvas, Paint paint) {
+    private void drawBackGround(Canvas canvas, Paint paint) {
+        mRect.set(0, 0, mWidth, mHeight);
+
         if (mGradient == null) {
             mGradient = new LinearGradient(0, 0, mWidth, mHeight,
                     new int[]{Color.parseColor("#DFE4FF"), Color.parseColor("#FFF0FF"), Color.parseColor("#FFEEED")},
                     new float[]{0, 0.8F, 1.F},
                     Shader.TileMode.CLAMP);
         }
-        mGradientPaint.setShader(mGradient);
-        mRect.set(0, 0, mWidth, mHeight);
-        canvas.drawRect(mRect, mGradientPaint);
+        // set background by shader
+        paint.setShader(mGradient);
+        canvas.drawRect(mRect, paint);
+        paint.setShader(null);
+
+        // set background by drawable
+//        mBgDrawable.setBounds(mRect);
+//        mBgDrawable.draw(canvas);
+
+        // draw rect
 //        drawRectByColor(canvas, paint, mRect, Color.parseColor("#55FF0000"));
     }
 
-    private void initLeftIcon(Canvas canvas, Paint paint) {
+    private void drawLeftIcon(Canvas canvas, Paint paint) {
         if (mLeftIconSize > mHeight) {
             mLeftIconRect.left = getPaddingLeft();
             mLeftIconRect.top = getPaddingTop();
@@ -197,11 +217,15 @@ public class CustomFeedTailView extends View {
             mLeftIconRect.right = mLeftIconRect.left + mLeftIconSize;
             mLeftIconRect.bottom = mHeight / 2 + mLeftIconSize / 2;
         }
-        canvas.drawBitmap(mLeftIcon, null, mLeftIconRect, mPaint);
+//        canvas.drawBitmap(mLeftIcon, null, mLeftIconRect, mPaint);
+
+        mLeftIconDrawable.setBounds(mLeftIconRect);
+        mLeftIconDrawable.draw(canvas);
+
 //        drawRectByColor(canvas, paint, mLeftIconRect, Color.parseColor("#5500FF00"));
     }
 
-    private void initRightIcon(Canvas canvas, Paint paint) {
+    private void drawRightIcon(Canvas canvas, Paint paint) {
         if (mRightIconSize > mHeight) {
             mRightIconRect.left = mWidth - getPaddingRight() - mHeight;
             mRightIconRect.top = getPaddingTop();
@@ -214,15 +238,13 @@ public class CustomFeedTailView extends View {
             mRightIconRect.bottom = mHeight / 2 + mRightIconSize / 2;
         }
         canvas.drawBitmap(mRightIcon, null, mRightIconRect, mPaint);
+
+        mRightIconDrawable.setBounds(mRightIconRect);
+        mRightIconDrawable.draw(canvas);
 //        drawRectByColor(canvas, paint, mRightIconRect, Color.parseColor("#550000FF"));
     }
 
-    private void initRightText(Canvas canvas, Paint paint) {
-        // init paint
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setColor(Color.parseColor("#FF03081A"));
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(0.5f); // 控制字体加粗的程度
+    private void drawRightText(Canvas canvas, Paint paint) {
         // draw text
         mRightTextRect.left = mRightIconRect.left - mRightTextRound.width() - ViewUtils.dpToPx(6);
         mRightTextRect.top = mHeight / 2 - mRightTextRound.height() / 2;
@@ -232,21 +254,15 @@ public class CustomFeedTailView extends View {
 //        drawRectByColor(canvas, paint, mRightTextRect, Color.parseColor("#5500FF00"));
     }
 
-    private void initText(Canvas canvas, Paint paint) {
-        // init paint
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setColor(Color.parseColor("#FF03081A"));
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(0.5f); // 控制字体加粗的程度
+    private void drawText(Canvas canvas, Paint paint) {
         // draw text
         mTextRect.left = mLeftIconRect.right + ViewUtils.dpToPx(7);
         mTextRect.top = mHeight / 2 - mTextBound.height() / 2;
         mTextRect.right = mRightTextRect.left;
         mTextRect.bottom = mHeight / 2 + mTextBound.height() / 2;
-        int textResidueWidth = mWidth - mLeftIconRect.width() - mRightIconRect.width() - mRightTextRect.width() - getPaddingLeft() - getPaddingRight();
-        if (mTextBound.width() > textResidueWidth) {
-            mTitle = TextUtils.ellipsize(mTitle, mTextPaint, (float) textResidueWidth,
-                    TextUtils.TruncateAt.END).toString();
+        float restWidth = mWidth - mLeftIconRect.width() - mRightIconRect.width() - mRightTextRect.width() - getPaddingLeft() - getPaddingRight();
+        if (mTextBound.width() > restWidth) {
+            mTitle = TextUtils.ellipsize(mTitle, mTextPaint, restWidth, TextUtils.TruncateAt.END).toString();
         }
         canvas.drawText(mTitle, mTextRect.left, getBaseLine(mTextRect, paint), paint);
 //        drawRectByColor(canvas, paint, mTextRect, Color.parseColor("#550000FF"));

4ed3ddbac8e07906e10e32a68458d74605c0aec8
lizhiping <lizhiping@autelrobotics.cn>
2022-10-08 10:45:18 +0800

    Revert "autel apt"
    This reverts commit 15d83fc63836cbb5e4163338bb304fbb129d37ac.

--- app/src/main/java/com/demo/apt/autel/AutelMessage.java
+++
@@ -1,9 +0,0 @@
-package com.demo.apt.autel;
-
-/**
- * Created by lizhiping on 2022/10/1.
- * <p>
- * description
- */
-public class AutelMessage {
-}
--- apt-annotation/src/main/java/com/tencent/wink/apt/annotation/AutelKey.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.wink.apt.annotation;
-
-/**
- * Created by lizhiping on 2022/10/1.
- * <p>
- * description
- */
-public @interface AutelKey {
-}
